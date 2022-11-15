b34760e918d4dd3cd35c34f6693f789ca6a7a81a
melekabbassi <abbassimelek@gmail.com>
2022-11-15 10:00:10 +0100

    add a controller and downgrade framworkcore, sqlserver to 6.0.10

---
+++ MySiteDAL/DataContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MySiteDAL
+{
+    internal class DataContextFactory : IDesignTimeDbContextFactory<MySiteContext>
+    {
+        public MySiteContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MySiteContext>();
+            optionsBuilder.UseSqlServer("server=(LocalDB)\\MSSQLLocalDB;Initial Catalog=DBIRM21;Integrated Security=true");
+
+            return new MySiteContext(optionsBuilder.Options);
+        }
+    }
+}
--- MySiteDAL/MySiteContext.cs
+++ MySiteDAL/MySiteContext.cs
@@ -8,21 +8,25 @@ using MySiteBL.Entities;
 
 namespace MySiteDAL
 {
-    internal class MySiteContext : DbContext
+    public class MySiteContext : DbContext
     {
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
-        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        //protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=(LocalDB)\\MSSQLLocalDB;Initial Catalog=DBIRM21;Integrated Security=true");
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    new DbInitializer(modelBuilder).Seed();
+        //}
+
+        public MySiteContext(DbContextOptions<MySiteContext> options) : base(options)
         {
-            optionsBuilder.UseSqlServer("server=(LocalDB)\\MSSQLLocalDB;Initial Catalog=DBIRM21;Integrated Security=true");
-        }
-        
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            new DbInitializer(modelBuilder).Seed();
         }
     }
 }
